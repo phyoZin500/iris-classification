3e7acd867dabd9c2ed9e148ff551b040a13d7454
phyoZin500 <140163577+phyoZin500@users.noreply.github.com>
2023-08-22 09:29:16 +0630

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,request
+import pickle
+
+
+
+model = pickle.load(open('lr_classifier.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    pred = ''
+    return render_template('index.html', **locals())
+
+    
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    if (request.method == "POST"):
+        p1 = float(request.form['p1'])
+        p2 = float(request.form['p2'])
+        p3 = float(request.form['p3'])
+        p4 = float(request.form['p4'])
+        pred = model.predict([[p1,p2,p3,p4]])[0]
+    return render_template('index.html', **locals())
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
